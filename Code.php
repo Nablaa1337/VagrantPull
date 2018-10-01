4a3f486b9d8b5b7c146265280bee3ff39d7aba51
Nablaa1337 <43674116+Nablaa1337@users.noreply.github.com>
2018-10-01 09:44:21 +0200

    Update Code.php

--- Code.php
+++ Code.php
@@ -1,8 +1,8 @@
 <?php
 $connection = mysql_connect('localhost', 'root', ''); //The Blank string is the password
-mysql_select_db('information_schema');
+mysql_select_db('employees');
 
-$query = "SELECT * FROM CHARACTER_SETS"; //You don't need a ; like you do in SQL
+$query = "SELECT * FROM employees"; //You don't need a ; like you do in SQL
 $result = mysql_query($query);
 
 echo "<table>"; // start a table tag in the HTML
